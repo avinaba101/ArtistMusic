dc3ebcf06babd46d48cb2806bb530543d7703dda
Artist <satyamshiv55@gmail.com>
2026-06-01 01:14:24 +0530

    Update _preload.py

--- Elevenyts/helpers/_preload.py
+++ Elevenyts/helpers/_preload.py
@@ -1,4 +1,18 @@
-# _preload.py - Background Track Preloading Manager
+# ==========================================================
+# Copyright (c) 2026 ArtistBots
+# All Rights Reserved.
+#
+# Project      : ArtistBots API Telegram Music Bot
+# Powered By   : Artist
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @ArtistApibot
+# Channel      : https://t.me/artistbots
+# GitHub       : https://github.com/elevenyts
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
 
 import asyncio
 import logging
