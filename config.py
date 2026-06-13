8cc9306e572e40c4d896b5d5db4a96cd04d039e3
idhack3282-max <idhack3282@gmail.com>
2026-06-13 22:53:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,8 +43,8 @@ class Config:
         self.SESSION3: str = getenv("STRING_SESSION3", "")
 
         # Support Links
-        self.SUPPORT_CHANNEL: str = getenv("SUPPORT_CHANNEL", "https://t.me/elevenytsmusic")
-        self.SUPPORT_CHAT: str = getenv("SUPPORT_CHAT", "https://t.me/elevenytschats")
+        self.SUPPORT_CHANNEL: str = getenv("SUPPORT_CHANNEL", "https://t.me/ShanuanimeChatting")
+        self.SUPPORT_CHAT: str = getenv("SUPPORT_CHAT", "https://t.me/Raj_6399")
 
         # Excluded Chats
         self.EXCLUDED_CHATS: List[int] = self._parse_excluded_chats()
@@ -68,10 +68,10 @@ class Config:
         self.COOKIES_URL: List[str] = self._parse_cookies()
 
         # Images
-        self.DEFAULT_THUMB: str = getenv("DEFAULT_THUMB", "https://files.catbox.moe/zlmv6v.png")
-        self.PING_IMG: str = getenv("PING_IMG", "https://files.catbox.moe/zlmv6v.png")
-        self.START_IMG: str = getenv("START_IMG", "https://files.catbox.moe/zlmv6v.png")
-        self.RADIO_IMG: str = getenv("RADIO_IMG", "https://files.catbox.moe/zlmv6v.png")
+        self.DEFAULT_THUMB: str = getenv("DEFAULT_THUMB", "https://iili.io/CCyliQf.jpg")
+        self.PING_IMG: str = getenv("PING_IMG", "https://iili.io/CCyliQf.jpg")
+        self.START_IMG: str = getenv("START_IMG", "https://iili.io/CCyliQf.jpg")
+        self.RADIO_IMG: str = getenv("RADIO_IMG", "https://iili.io/CCyliQf.jpg")
 
         # Moderation
         self.EXCLUDED_USERNAMES: List[str] = getenv("EXCLUDED_USERNAMES", "").split()
